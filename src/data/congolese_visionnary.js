561e8e7d15ef58e8c8b01e9687d91b2b82b1b747
Lucien Bangelesa <ir.bangelesa@gmail.com>
2025-10-01 19:00:05 +0100

    Adding visionnary speakrs

--- src/data/congolese_visionnary.js
+++ src/data/congolese_visionnary.js
@@ -31,10 +31,10 @@ const congoleseVisionnary = [
 {
   id: 112,
   name: "Elie Nkumbi",
-  title: "Chief of Staff to the Minister of Finance (DRC)",
-  topic: "Financial Expertise, Energy Sector Regulation, and Microfinance Consulting",
+  title: "Deputy Chief of Staff to the Minister of Interior (DRC)",
+  topic: "Governance, Security, and Energy Sector Expertise",
   bioKey: "elie_nkumbi",
-  detailedBio: "Elie Nkumbi is a **Civil Electrical Engineer** and a senior expert with a diverse background in finance, energy, and microfinance. He currently serves as the **Chief of Staff to the Minister of Finance** in the Democratic Republic of Congo. His professional experience includes work as an expert within the Ministry of Finance, demonstrating in-depth knowledge of public financial management. He is also a **Microfinance Consultant** for the African Development Bank (AfDB) and a technical expert for the **ARE (Autorité de Régulation du secteur de l'Électricité)**, specializing in electrical energy. Known also as a communicator, his expertise bridges technical engineering, financial strategy, and regulatory affairs.",
+  detailedBio: "Elie Nkumbi is a **Civil Electrical Engineer** and a senior expert with a diverse background in governance, finance, and energy. He currently serves as the **Deputy Chief of Staff to the Minister of Interior, Security, Decentralization and Customary Affairs** in the Democratic Republic of Congo. His professional experience also includes work as an expert within the Ministry of Finance, demonstrating knowledge of public financial management. He is also a **Microfinance Consultant** for the African Development Bank (AfDB) and a technical expert for the **ARE (Autorité de Régulation du secteur de l'Électricité)**, specializing in electrical energy. His expertise spans technical engineering, financial strategy, and regulatory affairs.",
   avatar: "/intervenants/Elie Nkumbi.png",
   social: {
     twitter: null,
